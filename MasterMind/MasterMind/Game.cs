e217a74cd7f9ebafd53b8c9808c3a176c8d29e1f
William Talley <williamtalley51@gmail.com>
2019-02-06 21:07:03 -0500

    Mastermind game project

---
+++ MasterMind/MasterMind/Game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind
+{
+    public class Game
+    {
+        private List<int> Numbers;
+        public int NumberOfTurns;
+        public bool WonGame;
+
+        public Game() //Call constructor to start new game
+        {
+            WonGame = false;
+            NumberOfTurns = 10;
+            Random rnd = new Random();
+            Numbers = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                Numbers.Add(rnd.Next(1, 7));
+            }
+        }
+
+        public void SetDifficulty(string difficulty)
+        {
+            switch(difficulty)
+            {
+                case "1":
+                    NumberOfTurns = 10;
+                    break;
+                case "2":
+                    NumberOfTurns = 5;
+                    break;
+                default:
+                    NumberOfTurns = 3;
+                    break;
+            }
+        }
+
+        public string TakeGuess(string Guess)
+        {
+            bool WonThisTime = true;
+            string result = String.Empty;
+            List<char> GuessList = Guess.ToCharArray().ToList<char>();
+            List<int> GuessIntList = new List<int>();
+            foreach(char c in GuessList)
+            {
+                GuessIntList.Add((int)c - '0');
+            }
+
+            for (int i=0; i< GuessIntList.Count(); i++)
+            {
+                if (GuessIntList[i] != Numbers[i] && !Numbers.Contains(GuessIntList[i])) //If number in the spot isn't right and list doesn't contain the number leave a blank
+                {
+                    WonThisTime = false;
+                    result += " ";
+                }
+                else if (GuessIntList[i] != Numbers[i] && Numbers.Contains(GuessIntList[i])) //if the number is in the list but the guess isn't in the right spor leave a dash
+                {
+                    WonThisTime = false;
+                    result += "-";
+                }
+                else //Number is in the list and in the right spot
+                    result += "+";
+            }
+            WonGame = WonThisTime;
+            return result;
+        }
+
+
+        
+
+
+    }
+}
---
+++ MasterMind/MasterMind/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Game newGame = new Game();
+                string Guess = string.Empty;
+                string Difficulty = string.Empty;
+                Console.WriteLine("1 - Easy (10 turns)");
+                Console.WriteLine("2 - Hard (5 turns)");
+                Console.WriteLine("3 - Extra Hard (3 turns)");
+                while (Difficulty != "1" && Difficulty != "2" && Difficulty != "3")
+                {
+                    Console.WriteLine("Please Select Difficulty between 1 and 3:");
+                    Difficulty = Console.ReadLine();
+                }
+
+
+                Console.WriteLine("+++++++++++++++++++++++++++++++");
+                Console.WriteLine("-------------------------------");
+                switch (Difficulty)
+                {
+                    case "1":
+                        Console.WriteLine("Welcome to Easy Mode, you get 10 turns");
+                        break;
+                    case "2":
+                        Console.WriteLine("Welcome to Hard Mode, you get 5 turns");
+                        break;
+                    default:
+                        Console.WriteLine("Welcome to Extra Hard Mode, you get 3 turns");
+                        break;
+                }
+                newGame.SetDifficulty(Difficulty);
+
+                while (newGame.NumberOfTurns > 0)
+                {
+                    if (newGame.NumberOfTurns <= 3 && newGame.NumberOfTurns > 1)
+                        Console.WriteLine("Only " + newGame.NumberOfTurns.ToString() + " turns left!");
+                    if (newGame.NumberOfTurns == 1)
+                        Console.WriteLine("Last turn!");
+
+                    Console.WriteLine("Please enter 4 numbers between 1 and 6:");
+                    Guess = Console.ReadLine();
+
+                    if (Guess.Length != 4 || !Guess.All(char.IsDigit))
+                    { Console.WriteLine("Please enter a string of 4 numeric characters only!"); }
+                    else
+                    {
+                        Console.WriteLine(newGame.TakeGuess(Guess));
+                        if (newGame.WonGame)
+                            break;
+                        else
+                            newGame.NumberOfTurns -= 1;
+                    }
+                }
+
+                if (newGame.WonGame)
+                    Console.WriteLine("Congrats!!!!");
+                else
+                    Console.WriteLine("You Lose!");
+
+                Console.Read();
+            }
+        }
+
+
+
+    }
+
+}
